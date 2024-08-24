526b5e7c90d712a901a3806302630b0974b12dd1
V-Priv <vpriv10@gmail.com>
2024-08-24 13:07:22 +0300

    made a classwork main

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
-class Warrior ():
-    def init(self, name, power, endurance, hair_color):
+class Warrior:
+    def __init__(self, name, power, endurance, hair_color):
         self.name = name
         self.power = power
         self.endurance = endurance
         self.hair_color = hair_color
 
     def sleep(self):
-     print(f"{self.name} Лег спать")
-     self.endurance = + 2
+        print(f"{self.name} лег спать")
+        self.endurance += 2
 
     def eat(self):
-     print(f"{self.name} Сел кушать")
-     self.power = +1
+        print(f"{self.name} сел кушать")
+        self.power += 1
 
     def hit(self):
-     print(f"{self.name} бьет кого-то")
-     self.endurance = - 6
+        print(f"{self.name} бьет кого-то")
+        self.endurance -= 6
 
     def walk(self):
-     print(f"{self.name} гуляет")
+        print(f"{self.name} гуляет")
 
     def info(self):
-     print(f"имя воина - {self.name}")
-     print(f"цвет волос воина - {self.hair_color}")
-     print(f"сила воина - {self.power}")
-     print(f"выносливость  воина - {self.endurance}")
-     
+        print(f"имя воина - {self.name}")
+        print(f"цвет волос воина - {self.hair_color}")
+        print(f"сила воина - {self.power}")
+        print(f"выносливость  воина - {self.endurance}")
+
+
+war1 = Warrior("Степа", 76, 54, "коричневый")
+war2 = Warrior("Егор", 45 , 23 , 'блондин')
+
+
+war1.sleep()
+war1.eat()
+war1.hit()
+war1.walk()
+war1.info()
+
+war2.sleep()
+war2.eat()
+war2.hit()
+war2.walk()
+war2.info()
+
+# print(war2.endurance)
+# print(war2.power)
+
